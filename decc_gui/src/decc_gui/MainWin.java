ab6ec6c05ca42fc7ba0a79a829967700241dc1ee
nyradr <nyradr@protonmail.com>
2016-04-03 19:48:45 +0200

    add IDeccUser callbacks into MainWin

--- decc_gui/src/decc_gui/MainWin.java
+++ decc_gui/src/decc_gui/MainWin.java
@@ -2,14 +2,58 @@ package decc_gui;
 
 import javax.swing.JFrame;
 
+import decc.IDeccUser;
+
 /**
  * Main window for DECC management
  * @author nyradr
  *
  */
-public class MainWin extends JFrame{
+public class MainWin extends JFrame implements IDeccUser{
 	
 	public MainWin(){
 		super();
+	}
+
+	@Override
+	public void onNewPeer(String host) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPeerDeco(String host) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNewCom(String comid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onComEnd(String comid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNewRoad(String comid, String hosta, String hostb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEroute(String comid, String hosta, String hostb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMess(String comid, String mess) {
+		// TODO Auto-generated method stub
+		
 	}
 }
