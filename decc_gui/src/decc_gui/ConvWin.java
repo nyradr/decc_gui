16086124a0aec164e67fb4d089e7b65d734844d9
nyradr <nyradr@protonmail.com>
2016-04-04 15:11:08 +0200

    ConvWin using MessagesBox and TextEntry

--- decc_gui/src/decc_gui/ConvWin.java
+++ decc_gui/src/decc_gui/ConvWin.java
@@ -9,6 +9,7 @@ import javax.swing.border.Border;
 
 import decc_gui.convwin.Message;
 import decc_gui.convwin.MessagesBox;
+import decc_gui.convwin.TextEntry;
 
 /**
  * Conversation window between 2 peers in DECC
@@ -18,15 +19,27 @@ import decc_gui.convwin.MessagesBox;
 public class ConvWin extends JFrame{
 	
 	MessagesBox messbox;
-	JPanel textEntry;
+	TextEntry textEntry;
 	
 	public ConvWin(){
 		super();
 		
+		this.setSize(400,  500);
 		
+		buildUi();
 	}
 	
 	private void buildUi(){
+		
+		this.getContentPane().setLayout(new BorderLayout());
+		
+		messbox = new MessagesBox();
+		this.getContentPane().add(messbox, BorderLayout.CENTER);
+		
+		textEntry = new TextEntry();
+		this.getContentPane().add(textEntry, BorderLayout.SOUTH);
+		
+		
 		
 	}
 }
