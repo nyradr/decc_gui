d6d8a3235bcfaf0e33914795cc69f2d05de0eaeb
nyradr <nyradr@protonmail.com>
2016-04-04 20:37:56 +0200

    add DeccInstance to MainWin, log message for each callbacks from the decc instance

--- decc_gui/src/decc_gui/MainWin.java
+++ decc_gui/src/decc_gui/MainWin.java
@@ -1,8 +1,12 @@
 package decc_gui;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
+import decc.DeccInstance;
 import decc.IDeccUser;
+import decc_gui.mainwin.LogTextArea;
 
 /**
  * Main window for DECC management
@@ -11,53 +15,66 @@ import decc.IDeccUser;
  */
 public class MainWin extends JFrame implements IDeccUser{
 	
+	private DeccInstance decc;
+	
+	private LogTextArea lta;
+
+	
 	public MainWin(){
 		super();
+		
+		try {
+			decc = new DeccInstance(4242, "Foo", this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		this.setSize(400,  200);
+		
+		build();
 	}
 	
 	private void build(){
 		
+		
+		lta = new LogTextArea();
+		this.getContentPane().add(lta);
+		
+		lta.log("hello");
 	}
 
 	@Override
 	public void onNewPeer(String host) {
-		// TODO Auto-generated method stub
-		
+		lta.log("New peer connected : " + host);
 	}
 
 	@Override
 	public void onPeerDeco(String host) {
-		// TODO Auto-generated method stub
-		
+		lta.log("Peer disconected : " + host);
 	}
 
 	@Override
 	public void onNewCom(String comid) {
-		// TODO Auto-generated method stub
-		
+		lta.log("New communication opened with COMID : " + comid);
 	}
 
 	@Override
 	public void onComEnd(String comid) {
-		// TODO Auto-generated method stub
-		
+		lta.log("Communication with COMID " + comid + " has end");
 	}
 
 	@Override
 	public void onNewRoad(String comid, String hosta, String hostb) {
-		// TODO Auto-generated method stub
-		
+		lta.log("New road traced with COMID " + comid + " from " + hosta + " to " + hostb);
 	}
 
 	@Override
 	public void onEroute(String comid, String hosta, String hostb) {
-		// TODO Auto-generated method stub
-		
+		lta.log("Road with COMID " + comid + " from " + hosta + " to " + hostb + " is now destroyed");
 	}
 
 	@Override
 	public void onMess(String comid, String mess) {
-		// TODO Auto-generated method stub
-		
+		lta.log("Message from the COMID " + comid + " received : " + mess);
 	}
 }
