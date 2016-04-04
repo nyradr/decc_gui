a0faff05a92ca1967fade114f27c97bac2484b60
nyradr <nyradr@protonmail.com>
2016-04-04 12:06:44 +0200

    init ConvWin functions

--- decc_gui/src/decc_gui/ConvWin.java
+++ decc_gui/src/decc_gui/ConvWin.java
@@ -1,6 +1,14 @@
 package decc_gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import decc_gui.convwin.Message;
+import decc_gui.convwin.MessagesBox;
 
 /**
  * Conversation window between 2 peers in DECC
@@ -9,7 +17,16 @@ import javax.swing.JFrame;
  */
 public class ConvWin extends JFrame{
 	
+	MessagesBox messbox;
+	JPanel textEntry;
+	
 	public ConvWin(){
 		super();
+		
+		
+	}
+	
+	private void buildUi(){
+		
 	}
 }
