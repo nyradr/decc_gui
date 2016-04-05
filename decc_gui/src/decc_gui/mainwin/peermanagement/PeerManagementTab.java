646d7bdfcc3381f379b9c61830694b6742b99c13
nyradr <nyradr@protonmail.com>
2016-04-05 20:25:52 +0200

    add PeerManagementTab (TODO : better display)

---
+++ decc_gui/src/decc_gui/mainwin/peermanagement/PeerManagementTab.java
@@ -0,0 +1,82 @@
+package decc_gui.mainwin.peermanagement;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import decc.DeccInstance;
+
+/**
+ * Main class for peer management tab
+ * @author nyradr
+ */
+public class PeerManagementTab extends JPanel{
+	
+	private DeccInstance decc;
+	
+	PeerList pl;
+	
+	JButton remPeer;
+	JButton addPeer;
+	
+	public PeerManagementTab(DeccInstance decc){
+		super();
+		
+		this.decc = decc;
+		
+		build();
+		buildCommandBox();
+	}
+	
+	private void build(){
+		this.setLayout(new BorderLayout());
+		
+		pl = new PeerList(decc.getIpPeer());
+		
+		this.add(pl, BorderLayout.CENTER);
+	}
+	
+	private void buildCommandBox(){
+		JPanel commandPanel = new JPanel();
+		commandPanel.setLayout(new BorderLayout());
+		
+		// remove peer button
+		remPeer = new JButton("Remove peer");
+		remPeer.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(pl.getSelectedPeer() != null)	// if selected element -> remove into decc
+					decc.disconnectPeer(pl.getSelectedPeer());
+			}
+		});
+		commandPanel.add(remPeer, BorderLayout.NORTH);
+		
+		// add peer button
+		addPeer = new JButton("Add peer");
+		addPeer.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// show input dialog for enter a ip(or host name)
+				String host = JOptionPane.showInputDialog("Type host name or IP of the peer to add");
+				
+				try{	// try connect and refresh display
+					decc.connect(host);
+					pl.resetValues(decc.getIpPeer());
+				}catch (UnknownHostException uhe){	// error
+					JOptionPane.showMessageDialog(null, "Impossible to reach the target", "Error", JOptionPane.ERROR_MESSAGE);
+				}catch (IOException ioe){
+					ioe.printStackTrace();
+				}
+			}
+		});
+		commandPanel.add(addPeer, BorderLayout.SOUTH);
+		
+		this.add(commandPanel, BorderLayout.EAST);
+	}
+}
