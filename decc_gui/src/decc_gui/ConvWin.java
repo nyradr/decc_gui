0f3643393dabc245052f736b240fd0ad9a25cee4
nyradr <nyradr@protonmail.com>
2016-04-06 18:35:39 +0200

    integration of decc in convWin

--- decc_gui/src/decc_gui/ConvWin.java
+++ decc_gui/src/decc_gui/ConvWin.java
@@ -7,6 +7,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
+import decc.DeccInstance;
+import decc.ICom;
+import decc_gui.convwin.ITextEntry;
 import decc_gui.convwin.Message;
 import decc_gui.convwin.MessagesBox;
 import decc_gui.convwin.TextEntry;
@@ -16,13 +19,19 @@ import decc_gui.convwin.TextEntry;
  * @author nyradr
  *
  */
-public class ConvWin extends JFrame{
+public class ConvWin extends JFrame implements ITextEntry{
 	
-	MessagesBox messbox;
-	TextEntry textEntry;
+	private DeccInstance decc;
+	private ICom conv;
 	
-	public ConvWin(){
+	private MessagesBox messbox;
+	private TextEntry textEntry;
+	
+	public ConvWin(DeccInstance decc, ICom conv){
 		super();
+		
+		this.decc = decc;
+		this.conv = conv;
 		
 		this.setSize(400,  500);
 		
@@ -31,15 +40,38 @@ public class ConvWin extends JFrame{
 	
 	private void buildUi(){
 		
+		this.setTitle(conv.getTargetName());
 		this.getContentPane().setLayout(new BorderLayout());
 		
 		messbox = new MessagesBox();
 		this.getContentPane().add(messbox, BorderLayout.CENTER);
 		
-		textEntry = new TextEntry();
+		textEntry = new TextEntry(this);
 		this.getContentPane().add(textEntry, BorderLayout.SOUTH);
-		
-		
-		
+	}
+
+	@Override
+	public void onEntry(String txt) {
+		if(txt.length() > 0){
+			decc.send(conv.getComid(), txt);
+			messbox.addMessage(txt, MessagesBox.SIDE_LEFT);
+		}
+	}
+	
+	/**
+	 * Display received message
+	 * @param mess message to display
+	 */
+	public void receiveMessage(String mess){
+		if(mess.length() > 0)
+			messbox.addMessage(mess, MessagesBox.SIDE_RIGHT);
+	}
+	
+	/**
+	 * Get the comid of the conversation
+	 * @return
+	 */
+	public String getComid(){
+		return conv.getComid();
 	}
 }
