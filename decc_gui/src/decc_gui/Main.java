64157f28944582cc365ff95c72b7966122f8315c
nyradr <nyradr@protonmail.com>
2016-04-06 18:36:53 +0200

    add program entry point (load MainWin)

--- decc_gui/src/decc_gui/Main.java
+++ decc_gui/src/decc_gui/Main.java
@@ -1,7 +1,16 @@
 package decc_gui;
 
+import java.awt.EventQueue;
+
 public class Main {
 	public static void main(String [] args){
-		
+		EventQueue.invokeLater(new Runnable() {
+	        
+            @Override
+            public void run() {
+                MainWin ex = new MainWin();
+                ex.setVisible(true);
+            }
+        });
 	}
 }
